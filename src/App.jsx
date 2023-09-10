6b7e5a3d8d8f1c018e8b87145f5088b47f81f727
Steven Abaco <stevenabaco@gmail.com>
2023-09-10 14:21:21 -0500

    Styled Component Props and "css" Function

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,30 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 function App() {
+	const StyledApp = styled.main`
+		background-color: orangered;
+		padding: 20px;
+	`;
 
-  const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow ;
-  `;
-  
-  const StyledApp = styled.main`
-  background-color: orangered;
-  padding: 20px;
-  `
-  
-  return (
+	return (
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>Hello World</H1>
+				<Heading as="h1">Hello World</Heading>
+				<Heading as="h2">Check in and out</Heading>
 				<Button onClick={() => alert("Check In")}>Check in</Button>
 				<Button onClick={() => alert("Check Out")}>Check in</Button>
+				<Heading as="h3">Form</Heading>
 				<Input
 					type="number"
 					placeholder="Number of quests"
 				></Input>
 			</StyledApp>
 		</>
-  );
+	);
 }
 
 export default App;
