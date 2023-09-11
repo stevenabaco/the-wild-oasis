d1f4103b75534f1c60f02dc24f24b29dfc51b59a
Steven Abaco <stevenabaco@gmail.com>
2023-09-10 19:15:54 -0500

    Build the App Layout Grid

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-import GlobalStyles  from "./styles/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
 	return (
@@ -15,39 +16,42 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route
-						index
-						element={
-							<Navigate
-								replace
-								to="dashboard"
-							/>
-						}
-					/>
-					<Route
-						path="dashboard"
-						element={<Dashboard />}
-					/>
-					<Route
-						path="bookings"
-						element={<Bookings />}
-					/>
-					<Route
-						path="cabins"
-						element={<Cabins />}
-					/>
-					<Route
-						path="users"
-						element={<Users />}
-					/>
-					<Route
-						path="settings"
-						element={<Settings />}
-					/>
-					<Route
-						path="account"
-						element={<Account />}
-					/>
+					<Route element={<AppLayout />}>
+						<Route
+							index
+							element={
+								<Navigate
+									replace
+									to="dashboard"
+								/>
+							}
+						/>
+						<Route
+							path="dashboard"
+							element={<Dashboard />}
+						/>
+						<Route
+							path="bookings"
+							element={<Bookings />}
+						/>
+						<Route
+							path="cabins"
+							element={<Cabins />}
+						/>
+						<Route
+							path="users"
+							element={<Users />}
+						/>
+						<Route
+							path="settings"
+							element={<Settings />}
+						/>
+						<Route
+							path="account"
+							element={<Account />}
+						/>
+					</Route>
+
 					<Route
 						path="login"
 						element={<Login />}
@@ -62,4 +66,4 @@ function App() {
 	);
 }
 
-export default App
+export default App;
