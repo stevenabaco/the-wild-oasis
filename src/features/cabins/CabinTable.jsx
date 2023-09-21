e3226fe9349f69ce2ca9618f74807b6c36edb98a
Steven Abaco <stevenabaco@gmail.com>
2023-09-21 08:47:13 -0500

    Add reusable context section

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner"
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,7 +27,8 @@ function CabinTable() {
   if(isLoading) return <Spinner />
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
         <div></div>
         <div>Cabin</div>
@@ -40,6 +42,8 @@ function CabinTable() {
         render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
       />
     </Table>
+    </Menus>
+    
   )
 }
 
