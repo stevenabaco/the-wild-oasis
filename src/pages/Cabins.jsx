6110b5cac5d35982093e586fe5a584aea27dd765
Steven Abaco <stevenabaco@gmail.com>
2023-09-24 12:36:22 -0500

    Add Client-Side Filtering and Sorting

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperation from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   
@@ -9,6 +10,7 @@ function Cabins() {
 		<>
 			<Row type="horizontal">
 				<Heading as="h1">All cabins</Heading>
+				<CabinTableOperation />
 			</Row>
 			<Row>
 				<CabinTable />
