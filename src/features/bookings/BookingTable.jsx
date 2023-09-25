d9ac34c7b664b6b7ee690f92dd38af337932b545
Steven Abaco <stevenabaco@gmail.com>
2023-09-25 13:43:38 -0500

    Add pagination and pre-fetching

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -6,9 +6,10 @@ import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 import BookingRow from "./BookingRow";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const {bookings, isLoading} = useBookings();
+  const {bookings, isLoading, count} = useBookings();
 
   if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resourceName={"bookings"} />;
@@ -31,6 +32,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
