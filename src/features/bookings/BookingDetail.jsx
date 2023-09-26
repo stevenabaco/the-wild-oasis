eeb60571d856e48c6736f215d53daceb8f08c4f7
Steven Abaco <stevenabaco@gmail.com>
2023-09-26 13:30:19 -0500

    Add feature to Check out

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,29 +12,32 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
-  display: flex;
-  gap: 2.4rem;
-  align-items: center;
+	display: flex;
+	gap: 2.4rem;
+	align-items: center;
 `;
 
 function BookingDetail() {
-  const {booking, isLoading} = useBooking();
-  const moveBack = useMoveBack();
-  const navigate = useNavigate();
+	const { booking, isLoading } = useBooking();
+	const { checkout, isCheckingOut } = useCheckout();
+	const moveBack = useMoveBack();
+	const navigate = useNavigate();
 
-  if (isLoading) return <Spinner />;
+	if (isLoading) return <Spinner />;
 
-  const { status, id: bookingId } = booking;
+	const { status, id: bookingId } = booking;
 
-  const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+	const statusToTagName = {
+		unconfirmed: "blue",
+		"checked-in": "green",
+		"checked-out": "silver",
+	};
 
-  return (
+	return (
 		<>
 			<Row type="horizontal">
 				<HeadingGroup>
@@ -48,10 +51,15 @@ function BookingDetail() {
 
 			<ButtonGroup>
 				{status === "unconfirmed" && (
+					<Button onClick={() => navigate(`/checkin/${bookingId}`)}>Check In</Button>
+				)}
+				{status === "checked-in" && (
 					<Button
-						onClick={() => navigate(`/checkin/${bookingId}`)}
+						icon={<HiArrowUpOnSquare />}
+						onClick={() => checkout(bookingId)}
+						disabled={isCheckingOut}
 					>
-						Check In
+						Check out
 					</Button>
 				)}
 				<Button
@@ -62,7 +70,7 @@ function BookingDetail() {
 				</Button>
 			</ButtonGroup>
 		</>
-  );
+	);
 }
 
 export default BookingDetail;
